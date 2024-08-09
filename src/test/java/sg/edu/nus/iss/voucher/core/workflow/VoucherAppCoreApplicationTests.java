ac5aa203efe6b5c246d26c7e04308dbabe40ec9d
Thet-Nandar-Aung <e1327955@u.nus.edu>
2024-08-10 00:47:10 +0800

    Initial Commit for Core MS.

---
+++ src/test/java/sg/edu/nus/iss/voucher/core/workflow/VoucherAppCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.voucher.core.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherAppCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
