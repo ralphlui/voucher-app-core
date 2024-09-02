d90ca4c9513c8f51e2254bc28ee0ef59316f4ae6
KhinYadanarNg <e0015793@u.nus.edu>
2024-09-02 23:00:59 +0800

    added storeclasses for controller, service and repository, entity layer

---
+++ src/main/java/sg/edu/nus/iss/voucher/core/workflow/controller/StoreController.java
@@ -0,0 +1,5 @@
+package sg.edu.nus.iss.voucher.core.workflow.controller;
+
+public class StoreController {
+
+}
---
+++ src/main/java/sg/edu/nus/iss/voucher/core/workflow/repository/StoreRepository.java
@@ -0,0 +1,5 @@
+package sg.edu.nus.iss.voucher.core.workflow.repository;
+
+public interface StoreRepository {
+
+}
